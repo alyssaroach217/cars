fb7f88887052e308b7f25cd851219d4f40b5b322
Alyssa Roach <aroach@chapman.edu>
2018-12-03 12:36:03 -0800

    Adds cars.sh and My_old_cars.txt

--- cars.sh
+++ cars.sh
@@ -1,24 +1,24 @@
 #! /bin/bash
 #cars.sh
 #Alyssa Roach
-CHOICE=continue
-while [ $CHOICE -ne Quit ]
+CONTINUE=1
+while [ $CONTINUE -eq 1 ]
 do
-	echo "1. Enter a car (E) \2. List the cars (L) \3. Quit (Q): "; read choice
-	case "$CHOICE" in
-		"E")
+	echo -n "1. Enter a car (E) \2. List the cars (L) \3. Quit (Q): "; read choice
+	case "$choice" in
+		"E") 
 		echo "What is the year of your car?: "
 		read YEAR
 		echo "What is the make of your car?: "
 		read MAKE		
 		echo "What is the model of your car?: "
 		read MODEL
-		$ echo "$YEAR:$MAKE:$MODEL" >> My_old_cars.txt;;
+		echo "$YEAR:$MAKE:$MODEL" >> My_old_cars.txt;;
 		"L")
-		$ sort My_old_cars.txt;;
+		sort My_old_cars.txt;;
 		"Q")
 		echo "Goodbye"
-		CHOICE = Quit
+		CONTINUE=0;;
 	esac
 done
 
